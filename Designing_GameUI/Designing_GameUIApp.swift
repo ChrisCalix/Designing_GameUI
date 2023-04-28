a658a6bc53150c1e30dc3d38c60799c9e5bdadde
christian.sonic <yoh.chrisitan@gmail.com>
2023-04-28 16:11:26 -0400

    Initial Commit

---
+++ Designing_GameUI/Designing_GameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Designing_GameUIApp.swift
+//  Designing_GameUI
+//
+//  Created by Sonic on 28/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Designing_GameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
